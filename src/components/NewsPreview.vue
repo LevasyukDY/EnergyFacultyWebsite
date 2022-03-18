99aef4a3f183f4a1a0a610166e813fbbf37b5bff
LevasyukDY <Levasyuk@gmail.com>
2022-03-19 04:32:14 +0900

    мелкие правки

--- src/components/NewsPreview.vue
+++ src/components/NewsPreview.vue
@@ -4,6 +4,7 @@
     <img src="@/assets/images/3.jpeg" alt="news" />
     <img src="@/assets/images/1.jpeg" alt="news" />
     <img src="@/assets/images/2.jpeg" alt="news" />
+    <br />
     <a href="#">Перейти к новостям...</a>
   </div>
 </template>
@@ -13,7 +14,7 @@
 <style scoped>
 img {
   width: 200px;
-  height: 100px;
+  height: 200px;
   margin: 0 15px 0 15px;
   max-height: 100%;
   object-fit: cover;
