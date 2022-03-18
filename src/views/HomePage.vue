f93aa4cc802f341e1c73150887b379166f5d43bc
LevasyukDY <Levasyuk@gmail.com>
2022-03-19 05:05:42 +0900

    добавлен тестовый блок студенческого деканата

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,6 +3,14 @@
     <ParallaxCarousel />
     <h1>Новости</h1>
     <NewsPreview />
+    <br />
+    <h1>Студенческий деканат</h1>
+    <img src="@/assets/images/позитрон.png" alt="pozitron" />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi harum
+      veniam nam sed natus rerum maiores fugit magni dolor? Explicabo architecto
+      numquam natus, autem laudantium sed voluptatibus non soluta voluptatum.
+    </p>
   </div>
 </template>
 
