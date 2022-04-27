2fab298a3ddf6612dceb38c644a996eccd2baa60
LevasyukDY <Levasyuk@gmail.com>
2022-04-28 04:46:24 +0900

    добавлен контент на страницу для абитуриентов

--- src/views/AbiturientsPage.vue
+++ src/views/AbiturientsPage.vue
@@ -1,5 +1,64 @@
 <template>
   <div class="abiturients">
-    <h1>This is an abiturients page</h1>
+    <img src="@/assets/images/приемная-комиссия.jpeg" />
+    <h1>Приёмная кампания 2022</h1>
+    <p>
+      Приём документов в приёмную комиссию с 20 июня 2022 года будет полностью
+      дистанционным. <br />
+      Сервис
+      <a href="http://zabgu.ru/php/page.php?query=podat%27_zayavlenie">
+        «Личный кабинет поступающего»
+      </a>
+      позволяет подать заявление на любую форму обучения по всем направлениям
+      подготовки всех уровней образования.
+    </p>
+    <p>
+      Вступительные испытания в вузе по общеобразовательным предметам (для
+      инвалидов, иностранных граждан и лиц, имеющих среднее профессиональное или
+      высшее образование), а также вступительные испытания творческой или
+      профессиональной направленности будут проводиться по утверждённому
+      расписанию с применением дистанционных технологий.
+    </p>
+    <p>
+      Вся информация об изменении порядка приёма в вузы будет оперативно
+      размещаться на официальном сайте нашего вуза в разделе «Приёмная кампания
+      2022» и в группе приёмной комиссии в социальной сети Вконтакте.
+    </p>
+    <div class="links">
+      <a href="http://entrant.zabgu.ru/?page_id=193">
+        Список документов для поступления
+      </a>
+      <a href="http://entrant.zabgu.ru/?page_id=16">Сроки приёма заявлений</a>
+      <a href="http://entrant.zabgu.ru/?page_id=4902">О медицинских справках</a>
+      <a href="http://entrant.zabgu.ru/?page_id=116">Приказы о зачислении</a>
+      <a href="http://entrant.zabgu.ru/?page_id=610">Статистика приёма</a>
+      <a href="http://entrant.zabgu.ru/?page_id=604">Платный приём</a>
+      <a href="http://entrant.zabgu.ru/?page_id=203">
+        Приём иностранных граждан
+      </a>
+      <a href="http://entrant.zabgu.ru/?page_id=12">Правила приёма</a>
+    </div>
   </div>
 </template>
+
+<style scoped>
+img {
+  box-shadow: #909090 3px 3px 25px;
+}
+
+.abiturients p {
+  text-align: justify;
+}
+
+.links {
+  display: flex;
+  flex-grow: 1;
+  flex-flow: row wrap;
+  justify-content: center;
+}
+
+a {
+  margin-right: 20px;
+  margin-bottom: 10px;
+}
+</style>
