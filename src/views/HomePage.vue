2fb0d704486693cb50bdadc417e216b3e3ddb13f
LevasyukDY <Levasyuk@gmail.com>
2022-06-02 21:41:46 +0900

    небольшой фикс + загатовка для страницы студентам

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -65,7 +65,7 @@ export default {
 iframe {
   border: 0;
   width: 100%;
-  height: 60vh;
+  height: 45vh;
   margin-bottom: 20px;
   border-radius: 20px;
 }
--- src/views/StudentsPage.vue
+++ src/views/StudentsPage.vue
@@ -1,51 +1,66 @@
 <template>
-  <h1>Ресурсы</h1>
   <div class="students">
-    <button>Календарный учебный график</button>
-    <button>Расписание занятий</button>
-    <button>Центр дистанционного образования</button>
-    <button>Нумерация корпусов</button>
-    <button>Расписание звонков</button>
-    <button>Стипендии, общежития, трудоустройство</button>
-    <button>Конференции и олимпиады</button>
-    <button>Библиотека ЗабГУ</button>
-    <button>Управление воспитательной и социальной работы</button>
-    <button>Справочная информация</button>
-    <button>Информация по трудоустройству</button>
-    <button>Образцы заявлений</button>
-    <button>Нормативные документы</button>
+    <div class="schedule">
+      <h1>Поиск расписания</h1>
+      <div class="search__schedule_by_group">
+        <input
+          class="search__input"
+          :value="search"
+          @input="search = $event.target.value"
+          type="text"
+          placeholder="Введите группу..."
+        />
+      </div>
+      <div class="search__schedule_by_teacher">
+        <input
+          class="search__input"
+          :value="search"
+          @input="search = $event.target.value"
+          type="text"
+          placeholder="Введите преподавателя..."
+        />
+      </div>
+    </div>
+    <div class="teachers">
+      <h1>Поиск преподавателя</h1>
+      <div class="search__teacher">
+        <input
+          class="search__input"
+          :value="search"
+          @input="search = $event.target.value"
+          type="text"
+          placeholder="Введите часть ФИО..."
+        />
+      </div>
+    </div>
   </div>
 </template>
 
+<script></script>
+
 <style scoped>
-.students {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  flex-flow: row wrap;
-  justify-content: center;
+.search__schedule_by_group {
+  margin-bottom: 10px;
 }
-
-button {
-  background-color: var(--bg-color);
-  box-shadow: var(--buttons-shadow) 1px 1px 10px;
-  border: 1px solid var(--sidebar-link-hover);
-  color: var(--font-color);
-  font-size: 16px;
-  width: 300px;
-  height: 100px;
+.search__input {
+  width: 30ch;
+  border: 2px solid var(--sidebar-link-hover);
   border-radius: 15px;
-  transition: 0.2s;
-  margin: 20px;
+  font-size: 16px;
+  padding: 2px 10px;
+  background-color: var(--bg-color);
+  color: var(--font-color);
 }
 
-button:hover {
-  transition: 0.2s;
-  background-color: var(--buttons-hover);
-  cursor: pointer;
-  width: 350px;
-  height: 150px;
-  font-size: 18px;
-  color: #fff;
+.search__input:focus {
+  border-color: var(--sidebar-link-hover);
+  outline: 0;
+  outline: thin dotted \9;
+  /* IE6-9 */
+
+  -webkit-box-shadow: inset 0 1px 1px #00000013,
+    0 0 8px var(--sidebar-link-active);
+  -moz-box-shadow: inset 0 1px 1px #00000013, 0 0 8px var(--sidebar-link-active);
+  box-shadow: inset 0 1px 1px #00000013, 0 0 8px var(--sidebar-link-active);
 }
 </style>
